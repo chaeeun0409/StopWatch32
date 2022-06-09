9cae3c133b05e7b381f5e11a7690e253b33fe97c
chaeeun0409 <s2017@e-mirim.hs.kr>
2022-06-09 13:11:39 +0900

    스톱워치에 여러 UI컨드를 부착, 타이머 동작 구현

--- StopWatch32/Form1.cs
+++ StopWatch32/Form1.cs
@@ -12,6 +12,10 @@ namespace StopWatch32
 {
     public partial class Form1 : Form
     {
+        private int elapsedTime;
+        Label lblStatus = new Label();
+        private object timer1;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,5 +25,46 @@ namespace StopWatch32
         {
 
         }
+
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbStatus_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text += "+";
+            tbStatus.Text += "+";
+            if (timer1.Enabled == false)
+            {
+                timer1.Enabled = true;
+            }
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            elapsedTime++;
+            lblStatus.Text = elapsedTime + " 초 경과";
+            tbStatus.Text = elapsedTime + " 초 경과";
+            toolStripProgressBar1 value = elapsedTime;
+
+
+
+        }
+    }
+
+    public class toolStripProgressBar1
+    {
     }
 }
